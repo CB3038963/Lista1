1817e6141d698e58b809fc00ba6f508b8ffebb8f
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:11:13 -0300

    Add files via upload

---
+++ Lista1Ex10/Lista1Ex10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double dolar = 5.71;
+            double real;
+            Console.Write("Digite o valor em Real que deseja converte: ");
+            real = double.Parse(Console.ReadLine());
+
+            double converte = dolar * real;
+            
+            Console.WriteLine("R${0} equivale a ${1} em Dolar.",real, converte);
+        }
+    }
+}
