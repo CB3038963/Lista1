ba01d170090047bb27d2f2df872504f752ce67f7
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:07:38 -0300

    Add files via upload

---
+++ Lista1Ex1/Lista1Ex1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baze;
+            Console.Write("Digite o valor da base do retangulo: ");
+            baze = double.Parse(Console.ReadLine());
+            double altura;
+            Console.Write("Digite o valor da altura do retangulo: ");
+            altura = double.Parse(Console.ReadLine());
+            double area = baze * altura;
+           
+            Console.WriteLine("A area total do retangulo de altura {0} e base {1} sera de {2}",altura,baze,area);
+
+        }
+    }
+}
