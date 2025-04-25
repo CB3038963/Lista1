e75fdbba376d12df83e3bee0467d819fbb235bcd
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:10:31 -0300

    Add files via upload

---
+++ Lista1Ex8/Lista1Ex8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double celsius;
+            Console.Write("Digite quantos graus celsius deseja converter em farenheit: ");
+            celsius = double.Parse(Console.ReadLine());
+
+            double farenh = celsius * 1.8 + 32;
+
+            Console.WriteLine("{0}° Graus celsius equivale a {1}° Graus em Farenheit.",celsius,farenh);
+
+
+        }
+    }
+}
