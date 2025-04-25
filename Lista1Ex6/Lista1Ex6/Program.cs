6263e861124b843bcf8fdeddc37ff8515d747ce7
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:09:47 -0300

    Add files via upload

---
+++ Lista1Ex6/Lista1Ex6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double primeiro;
+            double segundo;
+            Console.Write("Digite o Primeiro valor: ");
+            primeiro = double.Parse(Console.ReadLine());
+            Console.Write("Digite o Segundo valor: ");
+            segundo = double.Parse(Console.ReadLine());
+
+            double multi = primeiro * segundo;
+            double geometrica = Math.Sqrt(multi);
+
+            Console.WriteLine("A a media geometrica desses 2 valores sera {0}", geometrica);
+
+
+        }
+    }
+}
