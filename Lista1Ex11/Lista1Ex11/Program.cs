958ca340d5f1e43b47abee10ab4840621e4d1700
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:11:33 -0300

    Add files via upload

---
+++ Lista1Ex11/Lista1Ex11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double X;
+            Console.Write("Digite o valor de X: ");
+            X = double.Parse(Console.ReadLine());   
+            double Y;
+            Console.Write("Digite o valor de Y: ");
+            Y = double.Parse(Console.ReadLine());
+
+            double calculo = Math.Pow(X,Y);
+
+            Console.WriteLine("Quando X:{0} e é elevado a Y:{1} sera de {2}.",X,Y,calculo);
+            
+        }
+    }
+}
