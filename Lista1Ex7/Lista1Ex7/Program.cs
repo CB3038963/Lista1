bd74744a6c578fcb1c6dec5f5516c181dfa142e3
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:10:08 -0300

    Add files via upload

---
+++ Lista1Ex7/Lista1Ex7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double milhasM;
+            Console.Write("Digite quantas milhas maritimas deseja converter em KM: ");
+            milhasM = double.Parse(Console.ReadLine());
+
+            double KM = 1000;
+
+            double convert = milhasM * KM;
+
+            Console.WriteLine("{0} milhas maritimas equivale a {1}Km.", milhasM,convert);
+
+        }
+    }
+}
