2df7120a78df3c7e01234c6b5e6c2fe184465415
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:08:34 -0300

    Add files via upload

---
+++ Lista1Ex3/Lista1Ex3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double diagonal;
+            Console.Write("Digite o valor da diagonal do quadrado: ");
+            diagonal = double.Parse(Console.ReadLine());
+
+            double exp2 = 2;
+            double quad = Math.Pow(diagonal, exp2);
+            double area = quad / 2;
+
+            Console.WriteLine("A area do quadrado com diagonal {0} sera de {1}", diagonal, area);
+        }
+    }
+}
