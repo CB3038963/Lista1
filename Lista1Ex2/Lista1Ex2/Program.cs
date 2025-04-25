216dae50c3db1122659eba15bcb7c92e461d11ef
CB3038963 <ferraz.joana@aluno.ifsp.edu.br>
2025-04-25 01:08:08 -0300

    Add files via upload

---
+++ Lista1Ex2/Lista1Ex2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista1Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double aresta;
+            Console.Write("Digite a aresta do quadrado: ");
+            aresta = double.Parse(Console.ReadLine());
+            double area = aresta * aresta;
+            Console.WriteLine("A area do quadrado sera de {0}.",area);
+
+        }
+    }
+}
